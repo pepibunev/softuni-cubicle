04b7af4e7947226d7110725c651331899f62b3c5
pepibunev <peterbunev9@gmail.com>
2023-05-31 14:35:55 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 const express = require('express');
+
+const handlebarsConfig = require('./config/handlebarsConfig');
+const expressConfig = require('./config/expressConfig')
+const homeController = require('./controllers/homeController');
+
 const app = express();
-
-
 const PORT = 5000;
 
-//Handlebars config
-const handlebarsConfig = require('./config/handlebarsConfig');
 handlebarsConfig(app);
-
-//Express config
-const expressConfig = require('./config/expressConfig')
 expressConfig(app)
 
 //Routes
-app.get('/', (req, res) => {
-    res.render('index')
-});
+app.get('/', homeController.getHome);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
