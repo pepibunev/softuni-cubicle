2a8dd2a1c8ac2e5092413b04c45e6e251224e975
pepibunev <peterbunev9@gmail.com>
2023-05-31 16:46:37 +0300

    create page controller

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const expressConfig = require('./config/expressConfig')
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 const app = express();
 const PORT = 5000;
@@ -12,6 +13,7 @@ expressConfig(app)
 
 //Routes
 app.use(homeController);
+app.use('/cubes', cubeController);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
