ae2e0816eff9cc6e15c15f08f66e765dfdf5c9f1
pepibunev <peterbunev9@gmail.com>
2023-05-31 14:46:57 +0300

    updated about

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ handlebarsConfig(app);
 expressConfig(app)
 
 //Routes
-app.get('/', homeController.getHome);
+app.use(homeController);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
