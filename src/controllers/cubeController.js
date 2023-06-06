f777fff1d5d89f594c10cda40a6872aa636c397d
pepibunev <peterbunev9@gmail.com>
2023-06-06 18:43:42 +0300

    update details to show accessories

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -27,7 +27,6 @@ router.post('/create', async (req, res) => {
 
 router.get('/:cubeId/details', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
-     
 
     if (!cube) {
         return res.redirect('/404');
--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -20,7 +20,8 @@ exports.getAll = async (search, from, to) => {
     return result;
 };
 
-exports.getOne = (cubeId) => Cube.findById(cubeId); 
+exports.getOne = (cubeId) => Cube.findById(cubeId).populate('accessories');
+//exports.getOneWithAccessories = (cubeId) => this.getOne(cubeId).populate('accessories'); 
 
 exports.create = (cubeData) => {
     const cube = new Cube(cubeData);
