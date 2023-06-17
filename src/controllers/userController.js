9350372bee9368618324229684de067cb80ef4c6
pepibunev <peterbunev9@gmail.com>
2023-06-18 01:47:49 +0300

    save user with password validator

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
 
+const userManager = require('../managers/userManager');
+
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
+
+router.post('/register', async (req, res) => {
+    const { 
+        username, 
+        password, 
+        repeatPassword 
+    } = req.body;
+
+    await userManager.register({
+        username, 
+        password, 
+        repeatPassword
+    });
+
+    res.redirect('/users/login');
+});
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
 
 module.exports = router;
---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: {
+        type: String,
+        // validate: {
+        //     validator: function (value) {
+        //         return this.repeatPassword === value;
+        //     },
+        //     message: 'Passwords missmatch!'
+        // }
+    }
+});
+
+userSchema.virtual('repeatPassword')
+    .set(function (value) {
+        if( value !== this.password){
+            throw new mongoose.MongooseError('Passwords do not match!');
+        }
+    })
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
